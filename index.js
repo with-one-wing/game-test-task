4e34e7153ae078c7f61e6cdbf9addf01928c2834
Viktor Voitenko <viktor.voitenko@gmail.com>
2019-06-16 16:39:29 +0300

    fix: lint

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const START_BUBBLE_AMOUNT = 10;
-
 const BAD_HEALTH_MIN  = 3.5;
 const GOOD_HEALTH_MIN  = 7.5;
 const TICK_HEALTH_MIN = -4.05;
 const TICK_HEALTH_MAX = 5.01;
-
 let gameScore = 0;
 
-function randomIntFromInterval(min, max) // min and max included
-{
-    return Math.random() * (max - min) + min;
-}
-
-function randomString() {
-    return 'customer' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-}
-
+const randomIntFromInterval = (min, max) => Math.random() * (max - min) + min;
+const randomString = () => 'customer' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 const createBubble = (customerAmount, health, usedSubscriptionDays, lastContactedDays) =>  ({
         id: randomString(),
         customerAmount,
         health,
         usedSubscriptionDays,
         lastContactedDays
-
 });
-
-
-const createArea = () => {
-    return {
+const createArea = () =>  ({
         xSize: 360,
         ySize: 30,
         bubbles: new Array(START_BUBBLE_AMOUNT).fill(null).map(() => createBubble(
@@ -36,11 +23,10 @@ const createArea = () => {
             randomIntFromInterval(0, 360),
             randomIntFromInterval(0, 30),
         ))
-    }
-};
-
-function createBubbleElement(bubble, xArea, yArea, onBubbleClick) {
+    });
+const createBubbleElement = (bubble, xArea, yArea, onBubbleClick) => {
     let color = 'yellow';
+
     if (bubble.health <= BAD_HEALTH_MIN) {
         color = 'red';
     } else if (bubble.health >= GOOD_HEALTH_MIN) {
@@ -48,6 +34,7 @@ function createBubbleElement(bubble, xArea, yArea, onBubbleClick) {
     }
 
     const elemBubble = document.createElement('div');
+
     elemBubble.className = 'bubble';
     elemBubble.style.backgroundColor = color;
     elemBubble.addEventListener('click', () => {
@@ -55,38 +42,35 @@ function createBubbleElement(bubble, xArea, yArea, onBubbleClick) {
     });
 
     const {top, left} = calculateBubbleCoords(xArea, yArea, bubble);
+
     elemBubble.style.top = top;
     elemBubble.style.left = left;
     elemBubble.id = bubble.id;
 
     return elemBubble;
-}
-
-
-function getAreaSize(container) {
+};
+const getAreaSize = container  => {
     const xArea = container.offsetWidth  / area.xSize;
     const yArea = container.offsetHeight / area.ySize;
 
     return {xArea, yArea};
-}
-
-function calculateBubbleCoords(xArea, yArea, bubble) {
+};
+const calculateBubbleCoords = (xArea, yArea, bubble) => {
     const top = yArea * bubble.lastContactedDays;
     const left = xArea * bubble.usedSubscriptionDays;
 
     return {top, left};
-}
-
-function drawGameArea(area, options) {
+};
+const drawGameArea = (area, options) =>  {
     const container = document.getElementById('area-container');
-    getAreaSize(container);
-    const {xArea, yArea} = getAreaSize(container);
 
+    getAreaSize(container);
+
+    const {xArea, yArea} = getAreaSize(container);
     const children = area.bubbles.map(bubble => createBubbleElement(bubble, xArea, yArea, options.onBubbleClick));
 
     children.forEach(child => container.appendChild(child));
-}
-
+};
 const rerenderBubbleElem = (bubble, container) => {
     getAreaSize(container)
     const {xArea, yArea} = getAreaSize(container);
@@ -104,14 +88,12 @@ const rerenderBubbleElem = (bubble, container) => {
 
     elemBubble.style.top = top;
     elemBubble.style.left = left;
-}
-
+};
 const removeBubbleElem = (bubble) => {
     const elemBubble = document.getElementById(bubble.id);
     elemBubble.remove();
 };
-
-function startGame(area) {
+const startGame = (area) => {
     drawGameArea(area, { onBubbleClick({ id }){
             area.bubbles = area.bubbles.map((bubble) => {
                 if (bubble.id === id) {
@@ -124,7 +106,6 @@ function startGame(area) {
                 return  bubble;
             });
      }});
-
     const t = setInterval(() => {
         const container = document.getElementById('area-container');
 
@@ -155,10 +136,10 @@ function startGame(area) {
         });
 
     }, 1000);
+
     setTimeout(() => {clearInterval(t)},60 * 2 * 1000 )
-}
+};
 
 const area = createArea();
-startGame(area);
 
-console.log(JSON.stringify(area, null, 2));
+startGame(area);
